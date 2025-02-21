d3b45cc9031246dfbbd3c75c8a58243f21d569a0
armand <armand.lecomte@epitech.eu>
2025-02-22 00:31:35 +0100

    feat: 0.0.2: adding correct values in json

--- offres/app.py
+++ offres/app.py
@@ -16,18 +16,18 @@ def get_redis():
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
-    choice_id = request.cookies.get('choice_id')
-    if not choice_id:
-        choice_id = hex(random.getrandbits(64))[2:-1]
-        choice = None
-        if request.method == 'POST':
-            redis = get_redis()
-            choice = request.form['choice']
-            data = json.dumps({'choice_id': choice_id, 'choice': choice})
-            redis.rpush('choices', data)
-        resp = make_response(render_template(
-            'sites_offres.html'))
-        return resp
+    user_id = request.cookies.get('user_id')
+    if not user_id:
+        user_id = hex(random.getrandbits(64))[2:-1]
+    if request.method == 'POST':
+        redis = get_redis()
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        data = json.dumps({'user_id': user_id, 'first_name': first_name, 'last_name': last_name})
+        redis.rpush('applications', data)
+    resp = make_response(render_template(
+        'sites_offres.html'))
+    return resp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
